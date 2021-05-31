fafb6318cd99d0693e95e52ba30df6c7bd61322f
Amanda-Rodriguez <amanda.maye@hotmail.com>
2021-05-30 23:40:06 -0500

    guarde fuentes

--- ensayo.js
+++ ensayo.js
@@ -1,28 +1,12 @@
-window.onload=function (){
-//crea las variables para el mensaje de bienvenida
-var saludo = "hola loca ";
-var name = "Maye";
-var mensaje = ", por favor revise su orden";
-// concatene las tres variables para hacerlo
-var bienvenida = saludo + name + mensaje;
-// crea variables para algo mas
-var signo = "bananos";
-var tiles = signo.length;
-var subtotal = tiles * 5;
-var nose = 7;
-var total = subtotal + nose; 
-//Encontrar el elemento que tiene un id o greting
-
-var el = document.getElementById('saludo');
-el.textContent = bienvenida;
-var elsigno = document.getElementById('signousuario');
-elsigno.textContent = signo;
-var eltiles = document.getElementById('tiles');
-eltiles.textContent = tiles;
-var elsubtotal = document.getElementById('subtotal');
-elsubtotal.textContent = '$' + subtotal;
-var elsubtotal = document.getElementById('nose');
-elsubtotal.textContent = '$' + nose;
-var eltotal = document.getElementById('total');
-eltotal.textContent = '$' +total;
+var msj ='algo de 10 % descuento';
+function mensaje(){
+    var el=document.getElementById('mensaje');
+    el.textContent = msj;
 }
+mensaje();
+
+function calculateArea(w,h){
+    var area = w * h;
+    return area;
+}
+ver p= calculateArea(3,5);
