5adda378f629e261cd73cb005479d42779e33058
Amanda-Rodriguez <amanda.maye@hotmail.com>
2021-08-05 21:10:11 -0500

    primera version con animacion

--- jsmycv.js
+++ jsmycv.js
@@ -1,4 +1,20 @@
+let fija=document.getElementsByTagName('nav')[0];
+let oculto=false;
+
 window.addEventListener('scroll',function(e){
-    let fija=document.getElementById('titulo');
-    /*fija.style.transform='translate(0,-100px)';*/
+    let ypantalla=window.pageYOffset;
+   
+    if ((ypantalla>200)&& (!oculto)){
+        fija.classList.add("ocultarnav");
+        console.log('chao');
+        oculto=true;
+    }
+    if ((ypantalla<200)&& (oculto)){
+        oculto=false;
+        fija.classList.remove("ocultarnav");
+        console.log('hola');
+    }
 })
+
+/*hay un problema con la funcion window.pageYoffset no estoy segura como calcula la altura.
+Si recargo la pagina quiero que se vaya al inicio*/
