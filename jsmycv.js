7ebec7d31622b941986fd279bd52e246dd1f674f
Amanda-Rodriguez <amanda.maye@hotmail.com>
2021-08-12 20:57:33 -0500

    Rellenar informacion
    Coloque algo con mas sentido en el texto de la pagina web

--- jsmycv.js
+++ jsmycv.js
@@ -1,20 +1,20 @@
 let fija=document.getElementsByTagName('nav')[0];
-let oculto=false;
+let oculto=true;
 
-window.addEventListener('scroll',function(e){
-    let ypantalla=window.pageYOffset;
-   
-    if ((ypantalla>200)&& (!oculto)){
-        fija.classList.add("ocultarnav");
-        console.log('chao');
-        oculto=true;
-    }
-    if ((ypantalla<200)&& (oculto)){
-        oculto=false;
-        fija.classList.remove("ocultarnav");
-        console.log('hola');
-    }
-})
+    window.addEventListener('scroll',function(e){
+        let ypantalla=window.pageYOffset;
+    
+        if ((ypantalla>200)&& (!oculto)){
+            fija.classList.remove("mostrarnav");
+            console.log('chao');
+            oculto=true;
+        }
+        if ((ypantalla<200)&& (oculto)){
+            fija.classList.add("mostrarnav");
+            console.log('hola');
+            oculto=false;
+        }
+    })
 
 /*hay un problema con la funcion window.pageYoffset no estoy segura como calcula la altura.
 Si recargo la pagina quiero que se vaya al inicio*/
